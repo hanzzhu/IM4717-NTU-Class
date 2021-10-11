5f5a5d5158ac2d739cd5be306609b16e418c7be0
Han <hzhu009@e.ntu.edu.sg>
2021-10-11 20:02:36 +0800

    Case Study 3i

--- Week06/CaseStudy_3/js/jobs.js
+++ Week06/CaseStudy_3/js/jobs.js
@@ -1,63 +1,93 @@
-
 function validateName(){
     var name = document.getElementById('name').value;
-    // console.log(name);
+    if (name.length <= 0){
+        alert("Name should not be empty!")
+        return false
+    }
+    if (!name.replace(/\s/g, '').length) {
+        alert("Name should not only contain spaces!")
+        return false
+    }
     if (checkName(name)) {
-        // alphabet letters found
-        console.log("correct");
+        console.log("name is correct");
+        return true
     } else {
-        alert("Alphabets and spaces only!");
+        alert("The name should only contain alphabet characters and spaces!");
+        return false
     }
 }
 
 function checkName(input){
     var alphabets = /^[a-zA-Z\s]*$/;
-
-    return !!input.match(alphabets);
+    return input.match(alphabets);
 }
 
 function validateEmail(){
     var email = document.getElementById('email').value;
 
-    // if email have @
+    // check @
     if (email.indexOf('@') != -1){
-        // split email
         var username = email.split('@')[0];
         var domain = email.split('@')[1];
         console.log(username, domain);
 
-        //check if username contains hyphen and period
+        //check - and .
         if (username.indexOf('-') == -1){
-            alert("Please add hyphen '-' to user name");
+            alert("Please add hyphen '-' to your email's user name");
+            return false
         }
 
         if (username.indexOf('.') == -1){
-            alert("Please add dot '.' to user name");
+            alert("Please add dot '.' to your email's user name");
+            return false
         }
 
-        //check domain name
+        //check email domian name
         if(domain.indexOf('.') != -1){
-            var domainArr = domain.split('.');
+            var domainArray = domain.split('.');
 
-            // check the number of address extensions
-            if(domainArr.length < 2 || domainArr.length > 4){
-                alert('domain name contains two to four address extensions');
+            // check email address extension
+            if(domainArray.length < 2 || domainArray.length > 4){
+                alert('Email domain should contain 2 to 4 address extensions');
+                return false
             }
 
-            // check the last extension
-            if(domainArr[domainArr.length - 1].length < 2 || domainArr[domainArr.length - 1].length > 3){
-                alert('last extension must have two to three characters.');
+            // check last extension
+            if(domainArray[domainArray.length - 1].length < 2 || domainArray[domainArray.length - 1].length > 3){
+                alert('Email last extension should contain 2 to 3 characters.');
+                return false
             }
         } else {
-            alert('please input correct domain extension');
+            alert('Wrong email domain extension!');
+            return false
         }
     } else {
-        alert('please add @ to email');
+        alert('Please include @ in email!');
+        return false
     }
+    return true
+}
+
+function validateStartDate(){
+    var getDate = document.getElementById('date').value;
+    var startDate = new Date(getDate);
+    var todayDate = new Date(Date.now());
+    console.log('start:'+startDate)
+    console.log('today:'+todayDate)
+
+    if (startDate < todayDate){
+        alert("The  start  date  cannot  be  from today and the past! ");
+        return false
+    }
+    return true
 }
 
 function validateForm(){
-    validateName();
-    validateEmail();
-    //checkStartDate();
+    if (!validateName() || !validateEmail() || !validateStartDate()){
+        alert("Please check your input again ");
+        return false
+    }else{
+        return true
+    }
+
 }
